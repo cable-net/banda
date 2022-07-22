ffee2180b1eebb2eb108935dae893b3a5dcaa12d
betuzo <betotsol@gmail.com>
2022-07-22 10:53:14 -0500

    adding eslint

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-require('dotenv').config();
+require('dotenv').config()
 
-const healthRoutes = require('./routes/health');
+const healthRoutes = require('./routes/health')
 
-app.use('/api/health', healthRoutes);
+app.use('/api/health', healthRoutes)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`servidor andando en: ${PORT}`);
-});
+  console.warn(`servidor andando en: ${PORT}`)
+})
